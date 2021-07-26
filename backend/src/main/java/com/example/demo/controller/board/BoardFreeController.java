a4ce57fc3a2a4a4b052340a2bb58a231dad5d0c0
glory6807 <pjj2818@gmail.com>
2021-07-26 21:52:11 +0900

    jj commit

--- backend/src/main/java/com/example/demo/controller/board/BoardFreeController.java
+++ backend/src/main/java/com/example/demo/controller/board/BoardFreeController.java
@@ -29,7 +29,7 @@ public class BoardFreeController {
     }
 
     public ModelAndView test() {
-        ModelAndView mav = new ModelAndView();
+        ModelAndView mav = new ModelAndView(); 
 
         List<BoardFreeVo> testList = testService.selectTest();
         mav.addObject("list", testList);
