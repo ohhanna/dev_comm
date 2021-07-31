ea1e27784cbf0026d2cc7e8d7ab55fe38a8342f5
yeongwoo <ywc412@gmail.com>
2021-07-31 19:05:13 +0900

    템플릿 라이브러리 및 소스 추가

---
+++ frontend/src/components/Headers/IndexHeader.js
@@ -0,0 +1,72 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+/*eslint-disable*/
+import React from "react";
+
+// reactstrap components
+import { Container } from "reactstrap";
+
+// core components
+
+function IndexHeader() {
+  return (
+    <>
+      <div
+        className="page-header section-dark"
+        style={{
+          backgroundImage:
+            "url(" + require("assets/img/antoine-barres.jpg").default + ")",
+        }}
+      >
+        <div className="filter" />
+        <div className="content-center">
+          <Container>
+            <div className="title-brand">
+              <h1 className="presentation-title">Developer Community</h1>
+              <div className="fog-low">
+                <img
+                  alt="..."
+                  src={require("assets/img/fog-low.png").default}
+                />
+              </div>
+              <div className="fog-low right">
+                <img
+                  alt="..."
+                  src={require("assets/img/fog-low.png").default}
+                />
+              </div>
+            </div>
+            <h2 className="presentation-subtitle text-center">
+              ㅇㅅㅇ 뭘 할까
+            </h2>
+          </Container>
+        </div>
+        <div
+          className="moving-clouds"
+          style={{
+            backgroundImage:
+              "url(" + require("assets/img/clouds.png").default + ")",
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default IndexHeader;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,59 @@
+/*!
+
+=========================================================
+* Paper Kit React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
+
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+
+// styles
+import "bootstrap/scss/bootstrap.scss";
+import "assets/scss/paper-kit.scss?v=1.3.0";
+import "assets/demo/demo.css?v=1.3.0";
+// pages
+import Index from "views/Index.js";
+import NucleoIcons from "views/NucleoIcons.js";
+import LandingPage from "views/examples/LandingPage.js";
+import ProfilePage from "views/examples/ProfilePage.js";
+import RegisterPage from "views/examples/RegisterPage.js";
+// others
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/index" render={(props) => <Index {...props} />} />
+      <Route
+        path="/nucleo-icons"
+        render={(props) => <NucleoIcons {...props} />}
+      />
+      <Route
+        path="/landing-page"
+        render={(props) => <LandingPage {...props} />}
+      />
+      <Route
+        path="/profile-page"
+        render={(props) => <ProfilePage {...props} />}
+      />
+      <Route
+        path="/register-page"
+        render={(props) => <RegisterPage {...props} />}
+      />
+      <Redirect to="/index" />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
