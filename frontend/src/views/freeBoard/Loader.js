cc271d53de8ef5bfa74e89b6fc6b35f320509f25
glory6807 <pjj2818@gmail.com>
2021-08-28 23:53:46 +0900

    댓글 데이터 확인 및 뼈대 구성,,

--- frontend/src/views/freeBoard/Loader.js
+++ frontend/src/views/freeBoard/Loader.js
@@ -3,7 +3,11 @@ import ReactLoading from 'react-loading';
 
 const Loader = () => {
     return (
-        <div class="contentWrap"> <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}> <ReactLoading type="spin" color="#3d66ba" height={30} width={30} timeout={3000}/> </div> </div>
+        <div class="contentWrap"> 
+            <div style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}> 
+                <ReactLoading type="spin" color="#3d66ba" height={30} width={30} timeout={3000}/> 
+            </div> 
+        </div>
 
 
     );
